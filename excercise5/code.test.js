371cc389038bcbf732101719239595417c517c46
Souradeep Nanda <souradeep.nanda123@gmail.com>
2018-01-25 09:28:02 +0530

    excercise 5

--- excercise5/code.test.js
+++ excercise5/code.test.js
@@ -2,18 +2,34 @@ const callAndApply = require('./code');
 
 describe('When arguments are passed as', () => {
   it('argument list', () => {
-    const toBeApplied = {};
+    const toBeAppliedTo = {};
     const expectedObject = {
       a: 1,
       b: 2,
       c: 3,
     };
-    const fun = (aArg, bArg) => {
-      expect(callAndApply.caller());
-    };
+    function fun(aArg, bArg, cArg) {
+      this.a = aArg;
+      this.b = bArg;
+      this.c = cArg;
+    }
+    callAndApply.caller(toBeAppliedTo, fun, 1, 2, 3);
+    expect(toBeAppliedTo).toEqual(expectedObject);
   });
   it('argument array', () => {
-
+    const toBeAppliedTo = {};
+    const expectedObject = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+    function fun(aArg, bArg, cArg) {
+      this.a = aArg;
+      this.b = bArg;
+      this.c = cArg;
+    }
+    callAndApply.applier(toBeAppliedTo, fun, [1, 2, 3]);
+    expect(toBeAppliedTo).toEqual(expectedObject);
   });
 });
 
